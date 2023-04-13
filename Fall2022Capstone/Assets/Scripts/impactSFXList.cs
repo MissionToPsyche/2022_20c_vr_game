d08912226b0e5bdb56a8f4b9c1840a214e58d391
Antonio <armoren6@asu.edu>
2023-04-13 12:51:31 -0700

    Stable Release 1.0
    A stable .apk has been created and tested on Oculus Quest 1 and 2
    - build1 was tested on a Quest 1
    - build2 improves on build1 and was tested on a Quest 2
    
    -- Main features --
    - audio has been added to the interactables so that they make collision sounds on impact
    - bug fixed in build1 where menu wasn't appearing on button press by remapping it to the Y button
    - added more UI including credits, sponsor disclaimer, and more tutorials
    - resized hub area to be more proportional to player
    - added box colliders to the walls to add collision and prevent the player from moving through the walls
    
    -- Main issues --
    I, Antonio, accidently deleted my version of Unity that the project was running on so I had to downgrade my project. Therefore, I am making this push without pushing my project version or any project settings. Including the build settings.
    
    -- Future work --
    Polish up the graphics with better lighting

---
+++ Fall2022Capstone/Assets/Scripts/impactSFXList.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class impactSFXList : MonoBehaviour
+{
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = Random.Range(0.9f, 1f);
+    }
+  
+  AudioSource audioSource;
+  public AudioClip[] impactsound;
+  private float magnitudeChecker;
+
+  void OnCollisionEnter(Collision collision)
+  {
+      magnitudeChecker = collision.relativeVelocity.magnitude;
+      if (magnitudeChecker > 0.5f)
+      {
+          audioSource.PlayOneShot(impactsound[Random.Range(0,impactsound.Length)], magnitudeChecker/10);
+          
+      }
+  }
+}
